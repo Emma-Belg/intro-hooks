5e4a16e01c0d3ed18739c62cffe23d46b93c3cd5
emma <emma.wishart@live.com.au>
2020-06-10 13:04:58 +0200

    Updates

---
+++ precache-manifest.a96f785b8433f23b3d05d78bba63aabd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55cf8d61ea73a378403b80b10d78f0c1",
+    "url": "/intro-hooks/index.html"
+  },
+  {
+    "revision": "c92bfdad96279832b0df",
+    "url": "/intro-hooks/static/css/main.c4ecd98d.chunk.css"
+  },
+  {
+    "revision": "a461c862f4a80a95f07e",
+    "url": "/intro-hooks/static/js/2.05f4bc84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/intro-hooks/static/js/2.05f4bc84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c92bfdad96279832b0df",
+    "url": "/intro-hooks/static/js/main.0a01f5fc.chunk.js"
+  },
+  {
+    "revision": "2a87802b20b7412a9059",
+    "url": "/intro-hooks/static/js/runtime-main.59124a28.js"
+  }
+]);
